766b16e140d30f31986debf501899900e51a8cfa
Ben Johnson <benbjohnson@yahoo.com>
2024-02-14 15:02:26 -0700

    Add ErrNoAuthToken

--- api/api.go
+++ api/api.go
@@ -7,3 +7,5 @@ var ErrNotFound = errors.New("Not Found")
 
 // ErrUnknown - Error to return when an unknown server error occurs
 var ErrUnknown = errors.New("An unknown server error occurred, please try again")
+
+var ErrNoAuthToken = errors.New("No access token available. Please login with 'flyctl auth login'")
