f7c5766bbabc007c4bf282b4a0793fe37d19c833
gwuah <griffithawuah15@gmail.com>
2024-02-15 11:07:59 +0100

    add metadata apis to flaps

--- flaps/flaps_machines.go
+++ flaps/flaps_machines.go
@@ -438,3 +438,42 @@ func (f *Client) Uncordon(ctx context.Context, machineID string) (err error) {
 
 	return nil
 }
+
+func (f *Client) SetMetadata(ctx context.Context, machineID, key, value string) error {
+	endpoint := fmt.Sprintf("/%s/metadata/%s", machineID, key)
+
+	in := map[string]interface{}{
+		"value": value,
+	}
+
+	err := f.sendRequestMachines(ctx, http.MethodPost, endpoint, in, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to set metadata for %s: %w", machineID, err)
+	}
+
+	return nil
+}
+
+func (f *Client) GetMetadata(ctx context.Context, machineID string) (map[string]string, error) {
+	endpoint := fmt.Sprintf("/%s/metadata", machineID)
+
+	out := map[string]string{}
+
+	err := f.sendRequestMachines(ctx, http.MethodGet, endpoint, nil, &out, nil)
+	if err != nil {
+		return out, fmt.Errorf("failed to get metadata for %s: %w", machineID, err)
+	}
+
+	return out, nil
+}
+
+func (f *Client) DeleteMetadata(ctx context.Context, machineID, key string) error {
+	endpoint := fmt.Sprintf("/%s/metadata/%s", machineID, key)
+
+	err := f.sendRequestMachines(ctx, http.MethodDelete, endpoint, nil, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to delete metadata for %s: %w", machineID, err)
+	}
+
+	return nil
+}
