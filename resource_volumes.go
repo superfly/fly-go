d27be716f556a9bead16e13af8d2e8863ad3d8e7
tvdfly <108194999+tvdfly@users.noreply.github.com>
2024-04-27 01:00:13 -0700

    Additional volume queries & types (#51)

--- resource_volumes.go
+++ resource_volumes.go
@@ -1,6 +1,8 @@
 package fly
 
-import "context"
+import (
+	"context"
+)
 
 func (c *Client) GetAppNameFromVolume(ctx context.Context, volID string) (*string, error) {
 	query := `
@@ -26,4 +28,65 @@ query($id: ID!) {
 	}
 
 	return &data.Volume.App.Name, nil
+}
+
+func (c *Client) GetAppNameStateFromVolume(ctx context.Context, volID string) (*string, *string, error) {
+	query := `
+query GetAppNameStateFromVolume($id: ID!) {
+	volume: node(id: $id) {
+		... on Volume {
+			app {
+				name
+			}
+			state
+		}
+	}
+}
+	`
+
+	req := c.NewRequest(query)
+
+	req.Var("id", volID)
+	ctx = ctxWithAction(ctx, "get_app_name_state_from_volume")
+
+	data, err := c.RunWithContext(ctx, req)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &data.Volume.App.Name, &data.Volume.State, nil
+}
+
+func (c *Client) GetSnapshotsFromVolume(ctx context.Context, volID string) ([]VolumeSnapshot, error) {
+	query := `
+query GetSnapshotsFromVolume($id: ID!) {
+	volume: node(id: $id) {
+		... on Volume {
+			snapshots {
+				nodes {
+					id
+					size
+					digest
+					createdAt
+				}
+			}
+		}
+	}
+}
+	`
+
+	req := c.NewRequest(query)
+
+	req.Var("id", volID)
+	ctx = ctxWithAction(ctx, "get_snapshots_from_volume")
+
+	data, err := c.RunWithContext(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	var snapshots []VolumeSnapshot
+	for _, snapshot := range data.Volume.Snapshots.Nodes {
+		snapshots = append(snapshots, NewVolumeSnapshotFrom(snapshot))
+	}
+	return snapshots, nil
 }
