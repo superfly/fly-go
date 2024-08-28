55b266d35a0cd3b11b31b1fd0de12b1b135ea82f
Tim Newsham <tim@fly.io>
2024-08-28 10:38:11 -1000

    change secret type repr to strings

--- secrets_types.go
+++ secrets_types.go
@@ -1,28 +1,26 @@
 package fly
 
-//go:generate go run golang.org/x/tools/cmd/stringer@latest -type=SecretType
-type SecretType int32
-
 const (
-	AppSecret                      = SecretType(1)
-	VolumeEncryptionKey            = SecretType(2)
-	SECRET_TYPE_KMS_HS256          = SecretType(3)
-	SECRET_TYPE_KMS_HS384          = SecretType(4)
-	SECRET_TYPE_KMS_HS512          = SecretType(5)
-	SECRET_TYPE_KMS_XAES256GCM     = SecretType(6)
-	SECRET_TYPE_KMS_NACL_AUTH      = SecretType(7)
-	SECRET_TYPE_KMS_NACL_BOX       = SecretType(8)
-	SECRET_TYPE_KMS_NACL_SECRETBOX = SecretType(9)
-	SECRET_TYPE_KMS_NACL_SIGN      = SecretType(10)
+	// Secret types
+	AppSecret                      = "AppSecret"
+	VolumeEncryptionKey            = "VolumeEncryptionKey"
+	SECRET_TYPE_KMS_HS256          = "SECRET_TYPE_KMS_HS256"
+	SECRET_TYPE_KMS_HS384          = "SECRET_TYPE_KMS_HS384"
+	SECRET_TYPE_KMS_HS512          = "SECRET_TYPE_KMS_HS512"
+	SECRET_TYPE_KMS_XAES256GCM     = "SECRET_TYPE_KMS_XAES256GCM"
+	SECRET_TYPE_KMS_NACL_AUTH      = "SECRET_TYPE_KMS_NACL_AUTH"
+	SECRET_TYPE_KMS_NACL_BOX       = "SECRET_TYPE_KMS_NACL_BOX"
+	SECRET_TYPE_KMS_NACL_SECRETBOX = "SECRET_TYPE_KMS_NACL_SECRETBOX"
+	SECRET_TYPE_KMS_NACL_SIGN      = "SECRET_TYPE_KMS_NACL_SIGN"
 )
 
 type ListSecret struct {
-	Label string     `json:"label"`
-	Type  SecretType `json:"type"`
+	Label string `json:"label"`
+	Type  string `json:"type"`
 }
 
 type CreateSecretRequest struct {
-	Label string     `json:"label"`
-	Type  SecretType `json:"type"`
-	Value []byte     `json:"value,omitempty"`
+	Label string `json:"label"`
+	Type  string `json:"type"`
+	Value []byte `json:"value,omitempty"`
 }
