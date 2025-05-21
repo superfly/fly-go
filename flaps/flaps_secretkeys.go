907247c8c3f013c3d3d56f12db2ce7c0c1c35156
Tim Newsham <tim@fly.io>
2025-05-20 19:13:50 -1000

    add client support for new flaps secrets and secretkeys api endpoints

---
+++ flaps/flaps_secretkeys.go
@@ -0,0 +1,159 @@
+package flaps
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	fly "github.com/superfly/fly-go"
+)
+
+func (f *Client) sendRequestSecretkeys(ctx context.Context, method, endpoint string, in, out interface{}, qs url.Values, headers map[string][]string) error {
+	endpoint = fmt.Sprintf("/apps/%s/secretkeys%s", url.PathEscape(f.appName), endpoint)
+	if qs != nil {
+		endpoint += "?" + qs.Encode()
+	}
+	return f._sendRequest(ctx, method, endpoint, in, out, headers)
+}
+
+func (f *Client) ListSecretkeys(ctx context.Context, version *uint64) ([]fly.SecretKey, error) {
+	ctx = contextWithAction(ctx, secretkeysList)
+
+	var qs url.Values
+	if version != nil {
+		qs.Set("version", fmt.Sprintf("%d", *version))
+	}
+
+	out := fly.ListSecretkeysResp{}
+	if err := f.sendRequestSecretkeys(ctx, http.MethodGet, "", nil, &out, qs, nil); err != nil {
+		return nil, fmt.Errorf("failed to list secret keys: %w", err)
+	}
+
+	return out.Secrets, nil
+}
+
+func (f *Client) GetSecretkey(ctx context.Context, name string, version *uint64) (*fly.SecretKey, error) {
+	ctx = contextWithAction(ctx, secretkeyGet)
+
+	var qs url.Values
+	if version != nil {
+		qs.Set("version", fmt.Sprintf("%d", *version))
+	}
+
+	path := fmt.Sprintf("/%s", url.PathEscape(name))
+	out := fly.SecretKey{}
+	if err := f.sendRequestSecretkeys(ctx, http.MethodGet, path, nil, &out, qs, nil); err != nil {
+		return nil, fmt.Errorf("failed to get secret key: %w", err)
+	}
+
+	return &out, nil
+}
+
+func (f *Client) SetSecretkey(ctx context.Context, name string, typ string, value []byte) (*fly.SetSecretkeyResp, error) {
+	ctx = contextWithAction(ctx, secretkeySet)
+
+	path := fmt.Sprintf("/%s", url.PathEscape(name))
+	in := fly.SetSecretkeyRequest{Type: typ, Value: value}
+	out := fly.SetSecretkeyResp{}
+	if err := f.sendRequestSecretkeys(ctx, http.MethodPost, path, in, &out, nil, nil); err != nil {
+		return nil, fmt.Errorf("failed to set secret key: %w", err)
+	}
+
+	return &out, nil
+}
+
+func (f *Client) GenerateSecretkey(ctx context.Context, name string, typ string) (*fly.SetSecretkeyResp, error) {
+	ctx = contextWithAction(ctx, secretkeySet)
+
+	path := fmt.Sprintf("/%s/generate", url.PathEscape(name))
+	in := fly.SetSecretkeyRequest{Type: typ}
+	out := fly.SetSecretkeyResp{}
+	if err := f.sendRequestSecretkeys(ctx, http.MethodPost, path, in, &out, nil, nil); err != nil {
+		return nil, fmt.Errorf("failed to set secret key: %w", err)
+	}
+
+	return &out, nil
+}
+
+func (f *Client) DeleteSecretkey(ctx context.Context, name string) error {
+	ctx = contextWithAction(ctx, secretkeyDelete)
+
+	path := fmt.Sprintf("/%s", url.PathEscape(name))
+	if err := f.sendRequestSecretkeys(ctx, http.MethodDelete, path, nil, nil, nil, nil); err != nil {
+		return fmt.Errorf("failed to delete secret key: %w", err)
+	}
+
+	return nil
+}
+
+func (f *Client) EncryptSecretkey(ctx context.Context, name string, plaintext, assoc []byte, version *uint64) (*fly.EncryptSecretkeyResp, error) {
+	ctx = contextWithAction(ctx, secretkeyEncrypt)
+
+	var qs url.Values
+	if version != nil {
+		qs.Set("version", fmt.Sprintf("%d", *version))
+	}
+
+	path := fmt.Sprintf("/%s/encrypt", url.PathEscape(name))
+	in := fly.EncryptSecretkeyRequest{Plaintext: plaintext, AssocData: assoc}
+	out := fly.EncryptSecretkeyResp{}
+	if err := f.sendRequestSecretkeys(ctx, http.MethodPost, path, in, &out, qs, nil); err != nil {
+		return nil, fmt.Errorf("failed to encrypt with secret key: %w", err)
+	}
+
+	return &out, nil
+}
+
+func (f *Client) DecryptSecretkey(ctx context.Context, name string, ciphertext, assoc []byte, version *uint64) (*fly.DecryptSecretkeyResp, error) {
+	ctx = contextWithAction(ctx, secretkeyDecrypt)
+
+	var qs url.Values
+	if version != nil {
+		qs.Set("version", fmt.Sprintf("%d", *version))
+	}
+
+	path := fmt.Sprintf("/%s/decrypt", url.PathEscape(name))
+	in := fly.DecryptSecretkeyRequest{Ciphertext: ciphertext, AssocData: assoc}
+	out := fly.DecryptSecretkeyResp{}
+	if err := f.sendRequestSecretkeys(ctx, http.MethodPost, path, in, &out, qs, nil); err != nil {
+		return nil, fmt.Errorf("failed to decrypt with secret key: %w", err)
+	}
+
+	return &out, nil
+}
+
+func (f *Client) SignSecretkey(ctx context.Context, name string, plaintext []byte, version *uint64) (*fly.SignSecretkeyResp, error) {
+	ctx = contextWithAction(ctx, secretkeySign)
+
+	var qs url.Values
+	if version != nil {
+		qs.Set("version", fmt.Sprintf("%d", *version))
+	}
+
+	path := fmt.Sprintf("/%s/sign", url.PathEscape(name))
+	in := fly.SignSecretkeyRequest{Plaintext: plaintext}
+	out := fly.SignSecretkeyResp{}
+	if err := f.sendRequestSecretkeys(ctx, http.MethodPost, path, in, &out, qs, nil); err != nil {
+		return nil, fmt.Errorf("failed to sign with secret key: %w", err)
+	}
+
+	return &out, nil
+}
+
+func (f *Client) VerifySecretkey(ctx context.Context, name string, plaintext, sig []byte, version *uint64) error {
+	ctx = contextWithAction(ctx, secretkeyVerify)
+
+	var qs url.Values
+	if version != nil {
+		qs.Set("version", fmt.Sprintf("%d", *version))
+	}
+
+	path := fmt.Sprintf("/%s/verify", url.PathEscape(name))
+	in := fly.VerifySecretkeyRequest{Plaintext: plaintext, Signature: sig}
+	if err := f.sendRequestSecretkeys(ctx, http.MethodPost, path, in, nil, qs, nil); err != nil {
+		return fmt.Errorf("failed to verify with secret key: %w", err)
+	}
+
+	return nil
+}
--- flaps/flaps_secrets.go
+++ flaps/flaps_secrets.go
@@ -4,54 +4,71 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"net/url"
 
 	fly "github.com/superfly/fly-go"
 )
 
-func (f *Client) sendRequestSecrets(ctx context.Context, method, endpoint string, in, out interface{}, headers map[string][]string) error {
-	endpoint = fmt.Sprintf("/apps/%s/secrets%s", f.appName, endpoint)
+func (f *Client) sendRequestSecrets(ctx context.Context, method, endpoint string, in, out interface{}, qs url.Values, headers map[string][]string) error {
+	endpoint = fmt.Sprintf("/apps/%s/secrets%s", url.PathEscape(f.appName), endpoint)
+	if qs != nil {
+		endpoint += "?" + qs.Encode()
+	}
 	return f._sendRequest(ctx, method, endpoint, in, out, headers)
 }
 
-func (f *Client) ListSecrets(ctx context.Context) ([]fly.ListSecret, error) {
-	ctx = contextWithAction(ctx, secretsList)
+func (f *Client) ListAppSecrets(ctx context.Context, version *uint64) ([]fly.AppSecret, error) {
+	ctx = contextWithAction(ctx, appSecretsList)
 
-	out := make([]fly.ListSecret, 0)
-	if err := f.sendRequestSecrets(ctx, http.MethodGet, "", nil, &out, nil); err != nil {
-		return nil, fmt.Errorf("failed to list secrets: %w", err)
+	var qs url.Values
+	if version != nil {
+		qs.Set("version", fmt.Sprintf("%d", *version))
 	}
 
-	return out, nil
-}
-
-func (f *Client) CreateSecret(ctx context.Context, sLabel, sType string, in fly.CreateSecretRequest) error {
-	ctx = contextWithAction(ctx, secretCreate)
-
-	path := fmt.Sprintf("/%s/type/%s", sLabel, sType)
-	if err := f.sendRequestSecrets(ctx, http.MethodPost, path, in, nil, nil); err != nil {
-		return fmt.Errorf("failed to create secret: %w", err)
+	out := fly.ListAppSecretsResp{}
+	if err := f.sendRequestSecrets(ctx, http.MethodGet, "", nil, &out, qs, nil); err != nil {
+		return nil, fmt.Errorf("failed to list app secrets: %w", err)
 	}
 
-	return nil
+	return out.Secrets, nil
 }
 
-func (f *Client) GenerateSecret(ctx context.Context, sLabel, sType string) error {
-	ctx = contextWithAction(ctx, secretGenerate)
+func (f *Client) GetAppSecrets(ctx context.Context, name string, version *uint64) (*fly.AppSecret, error) {
+	ctx = contextWithAction(ctx, appSecretGet)
 
-	path := fmt.Sprintf("/%s/type/%s/generate", sLabel, sType)
-	if err := f.sendRequestSecrets(ctx, http.MethodPost, path, nil, nil, nil); err != nil {
-		return fmt.Errorf("failed to create secret: %w", err)
+	var qs url.Values
+	if version != nil {
+		qs.Set("version", fmt.Sprintf("%d", *version))
 	}
 
-	return nil
+	path := fmt.Sprintf("/%s", url.PathEscape(name))
+	out := fly.AppSecret{}
+	if err := f.sendRequestSecrets(ctx, http.MethodGet, path, nil, &out, qs, nil); err != nil {
+		return nil, fmt.Errorf("failed to get app secret: %w", err)
+	}
+
+	return &out, nil
 }
 
-func (f *Client) DeleteSecret(ctx context.Context, label string) error {
-	ctx = contextWithAction(ctx, secretDelete)
+func (f *Client) SetAppSecret(ctx context.Context, name string, value string) (*fly.SetAppSecretResp, error) {
+	ctx = contextWithAction(ctx, appSecretSet)
 
-	endpoint := fmt.Sprintf("/%s", label)
-	if err := f.sendRequestSecrets(ctx, http.MethodDelete, endpoint, nil, nil, nil); err != nil {
-		return fmt.Errorf("failed to delete secret: %w", err)
+	path := fmt.Sprintf("/%s", url.PathEscape(name))
+	in := fly.SetAppSecretRequest{Value: value}
+	out := fly.SetAppSecretResp{}
+	if err := f.sendRequestSecrets(ctx, http.MethodPost, path, in, &out, nil, nil); err != nil {
+		return nil, fmt.Errorf("failed to set app secret: %w", err)
+	}
+
+	return &out, nil
+}
+
+func (f *Client) DeleteAppSecret(ctx context.Context, name string) error {
+	ctx = contextWithAction(ctx, appSecretDelete)
+
+	path := fmt.Sprintf("/%s", url.PathEscape(name))
+	if err := f.sendRequestSecrets(ctx, http.MethodDelete, path, nil, nil, nil, nil); err != nil {
+		return fmt.Errorf("failed to delete app secret: %w", err)
 	}
 
 	return nil
