d5dae0cfcdbb027adc855e2a68a48bdf7e7b3e0e
Tim Newsham <tim@fly.io>
2025-05-21 09:07:16 -1000

    gofmt

--- secrets_types.go
+++ secrets_types.go
@@ -12,71 +12,71 @@ const (
 )
 
 type AppSecret struct {
-    Name   string  `json:"name"`
-    Value  *string `json:"value,omitempty"`
-    Digest string  `json:"digest"`
+	Name   string  `json:"name"`
+	Value  *string `json:"value,omitempty"`
+	Digest string  `json:"digest"`
 }
 
 type ListAppSecretsResp struct {
-    Secrets []AppSecret `json:"secrets"`
+	Secrets []AppSecret `json:"secrets"`
 }
 
 type SetAppSecretRequest struct {
-    Value string `json:"value"`
+	Value string `json:"value"`
 }
 
 type SetAppSecretResp struct {
-    AppSecret
-    Version uint64
+	AppSecret
+	Version uint64
 }
 
 type SecretKey struct {
-    Name      string `json:"name"`
-    Type      string `json:"type"`
-    Publickey []byte `json:"public_key,omitempty"`
+	Name      string `json:"name"`
+	Type      string `json:"type"`
+	Publickey []byte `json:"public_key,omitempty"`
 }
 
 type ListSecretkeysResp struct {
-    Secrets []SecretKey `json:"secret_keys"`
+	Secrets []SecretKey `json:"secret_keys"`
 }
 
 type SetSecretkeyRequest struct {
-    Type  string `json:"type"`
-    Value []byte `json:"value"`
+	Type  string `json:"type"`
+	Value []byte `json:"value"`
 }
 
 type SetSecretkeyResp struct {
-    SecretKey
-    Version uint64
+	SecretKey
+	Version uint64
 }
 
 type EncryptSecretkeyRequest struct {
-    Plaintext []byte `json:"plaintext"`
-    AssocData []byte `json:"associated_data,omitempty"`
+	Plaintext []byte `json:"plaintext"`
+	AssocData []byte `json:"associated_data,omitempty"`
 }
 
 type EncryptSecretkeyResp struct {
-    Ciphertext []byte `json:"ciphertext"`
+	Ciphertext []byte `json:"ciphertext"`
 }
 
 type DecryptSecretkeyRequest struct {
-    Ciphertext []byte `json:"ciphertext"`
-    AssocData  []byte `json:"associated_data,omitempty"`
+	Ciphertext []byte `json:"ciphertext"`
+	AssocData  []byte `json:"associated_data,omitempty"`
 }
 
 type DecryptSecretkeyResp struct {
-    Plaintext []byte `json:"plaintext"`
+	Plaintext []byte `json:"plaintext"`
 }
 
 type SignSecretkeyRequest struct {
-    Plaintext []byte `json:"plaintext"`
+	Plaintext []byte `json:"plaintext"`
 }
 
 type SignSecretkeyResp struct {
-    Signature []byte `json:"signature"`
+	Signature []byte `json:"signature"`
 }
 
 type VerifySecretkeyRequest struct {
-    Plaintext []byte `json:"plaintext"`
-    Signature []byte `json:"signature"`
+	Plaintext []byte `json:"plaintext"`
+	Signature []byte `json:"signature"`
 }
